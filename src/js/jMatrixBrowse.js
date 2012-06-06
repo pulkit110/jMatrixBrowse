4e6fb379b0d0c3c574a8d31846a81965d225050b
Pulkit Goyal <pulkit110@gmail.com>
2012-06-07 01:02:06 +0200

    Stopped using ZyngaSroller. Created scrolling using jQuery UI draggable. Moving divs that overflow at one end to the other end (like GMaps).

--- src/js/jMatrixBrowse.js
+++ src/js/jMatrixBrowse.js
@@ -32,14 +32,23 @@
 
     var _self = this;
     
-    var _settings;     // user Settings
-    var _api;          // API object
-    var _container;    // container for jMatrixBrowse
-    var _content;      // content of jMatrixBrowse
-    var _scroller;     // scroller object used for browsing
-    var _cellPosition; // cell position for the component
-    var _currentCell;  // currently shown cell (TODO: for now this cell is on left corner of matrix)
+    var _settings;      // user Settings
+    var _api;           // API object
+    var _container;     // container for jMatrixBrowse
+    var _content;       // content of jMatrixBrowse
+    var _cellPosition;  // cell position for the component
+    var _currentCell;   // currently shown cell (TODO: for now this cell is on left corner of matrix)
+    var _dragContainer; // Drag container that allows dragging using jQuery UI
+    var _cellElements;  // Array of array of cell elements.
 
+    // TODO: Move to separate class.
+    // Constants
+    var OVERFLOW_LEFT = 1;
+    var OVERFLOW_RIGHT = 2;
+    var OVERFLOW_TOP = 3;
+    var OVERFLOW_BOTTOM = 4;
+    var OVERFLOW_NONE = -1;
+    
     /**
      * Initialize the API
      * @param {String} type type of api: 'test' initializes the mockAPI
@@ -98,6 +107,7 @@
       }
     }
     
+    // TODO: Move utility functions to a separate class.
     /** 
      * Utility function that parses a position (row,col).
      * @returns {Object} position - Object representing the position denoted by the string. 
@@ -113,7 +123,35 @@
         };
       }
     }
-    
+
+    function checkOverflow(element, container) {
+      var containerOffset = container.offset();
+      var elementOffset = element.offset();
+      
+      var top = elementOffset.top - containerOffset.top;
+      var left = elementOffset.left - containerOffset.left;
+      var width = element.width();
+      var height = element.height();
+
+      if (left > container.width()) {
+        return OVERFLOW_RIGHT;
+      }
+      
+      if (top > container.height()) {
+        return OVERFLOW_BOTTOM;
+      }
+      
+      if (top+height < 0) {
+        return OVERFLOW_TOP;
+      }
+      
+      if (left+width < 0) {
+        return OVERFLOW_LEFT;
+      }
+      
+      return OVERFLOW_NONE;
+    }
+
     /**
      * Get user defined options from data-* elements.
      * @param {jQuery object} elem - the element to retrieve the user options from. 
@@ -206,12 +244,210 @@
     }
     
     /**
-     * Creates an empty matrix with size obtained from API and appends to content.
-     * @param {jQuery object} content - The element that acts as the matrix content.
-     * @param {jQuery object} container - The element that acts as the matrix container (parent of content).
+     * Create the drag container and make it draggable.
+     * @param {jQuery Object} container - container to attacht the content to.
+     * @returns {jQuery Object} dragContainer 
      */
-    function generateMatrixInDom(content, container) {
+    function createDragContainer(container) {
+      var dragContainer = jQuery(document.createElement('div'));
+      dragContainer.draggable({
+        drag: function (event, ui) {
+          dragHandler(event, ui);
+        }
+      });
+      container.append(dragContainer);
+      return dragContainer;
+    }
+    
+    /**
+     * Create the content div and append to container.
+     * @param {jQuery Object} container - container to attacht the content to.
+     * @returns {jQuery Object} content 
+     */
+    function createContentDiv(container) {
+      var content = jQuery(document.createElement('div'));
+      container.append(content);
+      return content;
+    }
 
+    /**
+     * Function that handles the drag event on dragContainer.
+     * @param {Object} event - Drag event.
+     * @param {Object} ui
+     */
+    function dragHandler (event, ui) {
+      checkAndRepositionCells(event, ui);
+    }
+    
+    /**
+     * Check and resposition cells that are overflowing.
+     * @param {Object} event - Drag event.
+     * @param {Object} ui
+     */
+    function checkAndRepositionCells() {
+      // TODO: We might need to check more elements in case of a quick drag.
+      
+      // Row on top might overflow from the top.
+      checkAndRepositionCellRow(_cellElements, _container, 0, OVERFLOW_TOP);
+      // Row on bottom might overflow from the bottom.
+      checkAndRepositionCellRow(_cellElements, _container, _cellElements.length-1, OVERFLOW_BOTTOM);
+      // Column on left might overflow from the left.
+      checkAndRepositionCellCol(_cellElements, _container, 0, OVERFLOW_LEFT);
+      // Column on right might overflow from the right.
+      checkAndRepositionCellCol(_cellElements, _container, _cellElements[0].length-1, OVERFLOW_RIGHT);
+    }
+    
+    /**
+     * Checks if the given row is overflowing and repositions if necessary. 
+     * @param {matrix} cellElements - The matrix of DOM objects representing cells.
+     * @param {jQuery Object} container - The container against which to check the overflow.
+     * @param {Number} row - Index of row to check the overflow for.
+     * @param {Number} overflow - Type of the overflow to check for.
+     */
+    function checkAndRepositionCellRow(cellElements, container, row, overflow) {
+      if (cellElements[row].length > 0 && isOverflowing(jQuery(cellElements[row][0]), container, overflow)) {
+        
+        var cellRow;
+        switch (overflow) {
+          case OVERFLOW_TOP:
+            // The row is overflowing from top. Move it to bottom. 
+            var height = cellElements.length;
+            var lastCell = (cellElements[height-1].length > 0) ? jQuery(cellElements[height-1][0]) : undefined;
+            if (lastCell === undefined) {
+              console.error('Unable to resposition row ' + row + ' overflowing from top.')
+              return;
+            }
+            // Change the position of all elements in the row.
+            var newTop = lastCell.position().top + lastCell.height();
+            for (var i = 0, w = cellElements[row].length; i < w; ++i) {
+              jQuery(cellElements[row][i]).css({
+                top: newTop
+              });
+            }
+            
+            // Move row in matrix to end
+            cellRow = cellElements.splice(row,1); // Remove row at [row]
+            if (cellRow.length > 0)
+              cellElements.push(cellRow[0]);  // Insert row at the end.
+            
+            break;
+
+          case OVERFLOW_BOTTOM:
+            // The row is overflowing from bottom. Move it to top.
+            var firstCell = (cellElements.length > 0 && cellElements[0].length > 0)?jQuery(cellElements[0][0]):undefined;
+            if (firstCell === undefined) {
+              console.error('Unable to resposition row ' + row + ' overflowing from bottom.')
+              return;
+            }
+            // Change the position of all elements in the row.
+            var newBottom = firstCell.position().top;
+            for (var i = 0, w = cellElements[row].length; i < w; ++i) {
+              jQuery(cellElements[row][i]).css({
+                top: newBottom - jQuery(cellElements[row][i]).height()
+              });
+            }
+            // Move row in matrix to first
+            cellRow = cellElements.splice(row,1);  // Remove row at [row]
+            if (cellRow.length > 0)
+              cellElements.splice(0,0,cellRow[0]);  // Insert row at the beginning.
+            
+            break;
+        }
+      }
+    }
+    
+    /**
+     * Checks if the given column is overflowing and repositions if necessary. 
+     * @param {matrix} cellElements - The matrix of DOM objects representing cells.
+     * @param {jQuery Object} container - The container against which to check the overflow.
+     * @param {Number} col - Index of column to check the overflow for.
+     * @param {Number} overflow - Type of the overflow to check for.
+     */
+    function checkAndRepositionCellCol(cellElements, container, col, overflow) {
+      if (isOverflowing(jQuery(cellElements[0][col]), container, overflow)) {
+        // TODO: Do error checks.
+        switch (overflow) {
+          case OVERFLOW_LEFT:
+            // The row is overflowing from left. Move it to right. 
+            if (cellElements.length <= 0 || cellElements[0].length <= 0) {
+              console.error('Unable to resposition col ' + col + ' overflowing from left.');
+              return;
+            }
+            // Change the position of all elements in the column.
+            var w = cellElements[0].length;
+            var lastCell = jQuery(cellElements[0][w-1]);
+            var newLeft = lastCell.position().left + lastCell.width();
+            for (var i = 0, h = cellElements.length; i < h; ++i) {
+              jQuery(cellElements[i][col]).css({
+                left: newLeft
+              });
+            }
+            // Move col to end in matrix.
+            for (var i = 0, h = cellElements.length; i < h; ++i) {
+              var cell = cellElements[i].splice(col, 1); // Remove element at [i][col]
+              cellElements[i].push(cell[0]); // Insert element at end of row i
+            }
+            break;
+
+          case OVERFLOW_RIGHT:
+            // The row is overflowing from right. Move it to left. 
+            if (cellElements.length <= 0 || cellElements[0].length <= 0) {
+              console.error('Unable to resposition col ' + col + ' overflowing from left.');
+              return;
+            }
+            var firstCell = jQuery(cellElements[0][0]);
+            // Change the position of all elements in the column.
+            var newRight = firstCell.position().left;
+            for (var i = 0, h = cellElements.length; i < h; ++i) {
+              jQuery(cellElements[i][col]).css({
+                left: newRight - jQuery(cellElements[i][col]).width()
+              });
+            }
+            // Move col to first in matrix.
+            for (var i = 0, h = cellElements.length; i < h; ++i) {
+              var cell = cellElements[i].splice(col, 1); // Remove element at [i][col]
+              cellElements[i].splice(0,0,cell[0]); // Insert element to [i][0]
+            }
+            break;
+        }
+      }
+    }
+    
+    /**
+     * Check if the given cell is overflowing the container dimensions. 
+     * @param {jQuery Object} element - jQuery object for the element to be checked for overflow.
+     * @param {jQuery Object} container - The container against which to check the overflow.
+     * @param {Number} overflow - Type of oevrflow to check.
+     * @returns true if there is an overflow. false otherwise.
+     */
+    function isOverflowing(element, container, overflow) {
+      var containerOffset = container.offset();
+      var elementOffset = element.offset();
+      
+      var top = elementOffset.top - containerOffset.top;
+      var left = elementOffset.left - containerOffset.left;
+      var width = element.width();
+      var height = element.height();
+
+      switch (overflow) {
+        case OVERFLOW_LEFT: 
+          return (left+width < 0);
+        case OVERFLOW_RIGHT: 
+          return (left > container.width());
+        case OVERFLOW_TOP: 
+          return (top+height < 0);
+        case OVERFLOW_BOTTOM: 
+          return (top > container.height());
+      }
+      return false;
+    }
+
+    /**
+     * Creates an empty matrix with size obtained from API and appends to content.
+     * @param {jQuery object} container - The element that acts as the matrix container (element that invoked jMatrixBrowse).
+     * @returns {jQuery object} content where matrix is generated.
+     */
+    function generateInitialMatrixContent(container) {
       var size = getMatrixSize();
       if (size == undefined) {
         console.error("Unable to get matrix size");
@@ -224,41 +460,43 @@
         return;
       }
       
-      var w = container.width();
-      var h = container.height();
-      var cellWidth = Math.round(container.width()/windowSize.width);
-      var cellHeight = Math.round(container.height()/windowSize.height);
+      var content = createContentDiv(container);
+      content.css({
+        'position' : 'absolute',
+        'top' : 0,
+        'left' : 0
+      });
       
-      content.width(cellWidth*size.width);
-      content.height(cellHeight*size.height);
+      var cellHeight = _container.height()/windowSize.height;
+      var cellWidth = _container.width()/windowSize.width;
       
       var windowPosition = getWindowPosition();
       
-      // TODO: Idea: Generate only the visible part of matrix. 
-      // Still set the content height and width of content so 
-      // that Zynga scroller scrolls the other parts 
-      
-      // TODO: Remove hardcoded variables
-      // Generate matrix content
+      _cellElements = [];
+      // Generate matrix content for only the rows that are in the window.
       var frag = document.createDocumentFragment();
-      for (var row=windowPosition.row; row <= windowPosition.row+windowSize.height; row++) {
-        for (var col=windowPosition.col; col <= windowPosition.col + windowSize.width; col++) {
-          elem = document.createElement("div");
+      for (var row=windowPosition.row; row < windowPosition.row+windowSize.height; row++) {
+        _cellElements.push([]);
+        for (var col=windowPosition.col; col < windowPosition.col + windowSize.width; col++) {
+          var elem = document.createElement("div");
           elem.style.backgroundColor = row%2 + col%2 > 0 ? "#ddd" : "";
           elem.style.width = cellWidth + "px";
           elem.style.height = cellHeight + "px";
+          elem.style.position = "absolute";
+          elem.style.top = (row-windowPosition.row)*cellHeight + "px";
+          elem.style.left = (col-windowPosition.col)*cellWidth + "px";
           elem.style.display = "inline-block";
           elem.style.textIndent = "6px";
           elem.innerHTML = row + "," + col;
-          elem.className += " " + generateClassNameForCell(row, col);
+          elem.className += " jMatrixBrowse-cell " + generateClassNameForCell(row, col);
           frag.appendChild(elem);
+          _cellElements[row-windowPosition.row].push(elem);
         }
-        elem = document.createElement("br");
-        frag.appendChild(elem);
       }
       content.append(frag);
+      return content;
     }
-
+    
     /**
      * Initialize the jMatrixBrowse.
      * @param {jQuery object} elem - the element to which to attach the jMatrixBrowse.
@@ -275,21 +513,17 @@
       initApi('test');
 
       _container = elem;
-      _content = jQuery(document.createElement('div'));
-      _container.append(_content);
       
-      // Generate matrix content and add to DOM
-      generateMatrixInDom(_content, _container);
-
-      // Attach EasyScroller to elem
-      _scroller = new EasyScroller(_content[0]);
+      // Create draggable area and add matrix to it.
+      _dragContainer = createDragContainer(_container);
       
-      // TODO: Use matrix data instead of indices generated in previous step.
+      // Generate initial content
+      _content = generateInitialMatrixContent(_dragContainer);
       
       // Scroll to the initial position
       var windowPosition = getWindowPosition();
       _self.scrollTo(windowPosition.row, windowPosition.col);
-
+      
       // Load data
       _self.reloadData();
     }
@@ -320,7 +554,6 @@
         row: row,
         col: col
       };
-      _scroller.scroller.scrollTo(_cellPosition.left, _cellPosition.top);
     };
     
     /**
@@ -337,7 +570,8 @@
       if (response && response.data) {
         for (var i = 0; i < response.data.length; ++i) {
           for (var j = 0; j < response.data[i].length; ++j) {
-            var cellData = response.data[i][j]; // TODO: If we support named methods, the data should be extracted for the named method corresponding to current layer.
+            // TODO: If we support named methods, the data should be extracted for the named method corresponding to current layer.
+            var cellData = response.data[i][j]; 
             var row = cellWindow.row1 + i;
             var col = cellWindow.col1 + j;
             jQuery('.' + generateClassNameForCell(row, col)).html(cellData);  
