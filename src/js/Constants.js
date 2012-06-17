83a1994ae6eab7c84bc3eb42705ef2b54baa1919
Pulkit Goyal <pulkit110@gmail.com>
2012-06-18 00:23:47 +0200

    Added constants for jMatrixBrowse.

---
+++ src/js/Constants.js
@@ -0,0 +1,18 @@
+/**
+ * @fileOverview Contains the jMatrixBrowse constants.
+ * @version 0.1
+ * @author Pulkit Goyal <pulkit110@gmail.com> 
+*/
+/**
+ * Constants for jMatrixBrowse.
+ * 
+ * @class Constants
+ */
+Constants = {
+  OVERFLOW_LEFT : 1,
+  OVERFLOW_RIGHT : 2,
+  OVERFLOW_TOP : 3,
+  OVERFLOW_BOTTOM : 4,
+  OVERFLOW_NONE : -1,
+  CLASS_BASE : 'jmatrixbrowse'
+};
