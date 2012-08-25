626d19e7dac8d0f3aa7d50c5281c08fde5410297
Pulkit Goyal <pulkit110@gmail.com>
2012-08-25 20:25:27 +0200

    Changed format of response for each cell for the demo.

--- demo/basic/js/jMatrixBrowseDemo.js
+++ demo/basic/js/jMatrixBrowseDemo.js
@@ -7,5 +7,21 @@
     var jMatrixBrowse;
     //create a new jMatrixBrowse
     var jMatrixBrowse = $('#my_browser').jMatrixBrowse();
+
+    jMatrixBrowse.parseResponseToString = function(response) {
+    	if (response !== null && response !== undefined) {
+        if( Object.prototype.toString.call(response) === '[object Array]' ) {
+          var result = '';
+          for (var i = 0; i < response.length; i++) {
+            result += '<span author="' + response[i].author + '">' + response[i].value + '</span>:';
+          };
+          return result;
+        } else {
+          return JSON.stringify(response);
+        }
+      }
+      return '';
+    }
+    
   });
 })(jQuery);
